c91a9c8fb4e6e309c6f6e9e5251deddbd075e4be
devang496 <devangsathwara496@gmail.com>
2024-09-13 13:27:31 +0530

    New changes done

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-let boxes = document.getElementsByClassName("box");
-// console.log(boxes);
-
-let playerO = true;
-
-let winnerPlayer = document.getElementById("winner");
-
+const boxes = document.getElementsByClassName("box");
+const winnerHeading = document.getElementById("winner");
 const newGameBtn = document.getElementById("new-game");
+let turnO = false;
 
-const scoreO = document.getElementById("scoreO");
-console.log(scoreO.innerText);
-const scoreX = document.getElementById("scoreX");
+let winO = document.getElementById("scoreO");
+let winX = document.getElementById("scoreX");
 
-// const winnerNamePlate = document.getElementById("winnerNamePlate");
-// console.log(winnerNamePlate);
+let countO = 0;
+let countX = 0;
 
-const winningPatternds = [
+const winPatterns = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -25,44 +20,65 @@ const winningPatternds = [
   [2, 4, 6],
 ];
 
-let countO = 0;
-let countX = 0;
-
 for (let box of boxes) {
   box.addEventListener("click", () => {
-    if (playerO === true) {
-      //   console.log("Clicked");
+    if (turnO === true) {
       box.innerText = "O";
-      playerO = false;
       box.setAttribute("disabled", "true");
+      turnO = false;
     } else {
-      //   console.log("Clicked");
       box.innerText = "X";
-      playerO = true;
       box.setAttribute("disabled", "true");
+      turnO = true;
     }
     checkWinner();
   });
 }
 
+newGameBtn.addEventListener("click", () => {
+  for (let box of boxes) {
+    box.innerText = "";
+    box.removeAttribute("disabled");
+  }
+  winnerHeading.classList.add("hidden");
+  newGameBtn.classList.add("hidden");
+});
+
+const disableAllBoxes = () => {
+  for (let box of boxes) {
+    box.setAttribute("disabled", "true");
+  }
+};
+
+const showWinner = (winnerPlayer) => {
+  winnerHeading.innerText = `Winner : ${winnerPlayer}`;
+  winnerHeading.classList.remove("hidden");
+  newGameBtn.classList.remove("hidden");
+
+  if (winnerPlayer === "O") {
+    countO++;
+    scoreO.innerText = `O : ${countO}`;
+  } else {
+    countX++;
+    scoreX.innerText = `X : ${countX}`;
+  }
+
+  disableAllBoxes();
+};
+
 const checkWinner = () => {
-  winningPatternds.forEach((pattern) => {
-    // console.log(pattern);/
-    // console.log(pattern[0]);
-    // console.log(pattern[1]);
+  winPatterns.forEach((pattern) => {
+    const index0Value = pattern[0];
+    const index1Value = pattern[1];
+    const index2Value = pattern[2];
 
-    // const index0Value = pattern[0];
-    // const index1Value = pattern[1];
-    // const index2Value = pattern[2];
+    const pos0 = boxes[index0Value];
+    const pos1 = boxes[index1Value];
+    const pos2 = boxes[index2Value];
 
-    // // console.log(boxes[pattern[1]]);
-    // const pos0 = boxes[index0Value];
-    // const pos1 = boxes[index1Value];
-    // const pos2 = boxes[index2Value];
-
-    const pos0Value = boxes[pattern[0]].innerText;
-    const pos1Value = boxes[pattern[1]].innerText;
-    const pos2Value = boxes[pattern[2]].innerText;
+    const pos0Value = pos0.innerText;
+    const pos1Value = pos1.innerText;
+    const pos2Value = pos2.innerText;
 
     if (pos0Value && pos1Value && pos2Value) {
       if (pos0Value === pos1Value && pos1Value === pos2Value) {
@@ -71,36 +87,3 @@ const checkWinner = () => {
     }
   });
 };
-
-const showWinner = (Player) => {
-  //   console.log(Player);
-  winnerPlayer.innerText = `Winner : ${Player}`;
-  winnerPlayer.classList.remove("hidden");
-  newGameBtn.classList.remove("hidden");
-  // winnerNamePlate.classList.remove("hidden");
-
-  if (Player === "O") {
-    countO++;
-    scoreO.innerText = `O : ${countO}`;
-  } else {
-    countX++;
-    scoreX.innerText = `X : ${countX}`;
-  }
-  disableAllBoxes();
-};
-
-const disableAllBoxes = () => {
-  for (let box of boxes) {
-    box.setAttribute("disabled", "true");
-  }
-};
-
-newGameBtn.addEventListener("click", () => {
-  for (let box of boxes) {
-    box.innerText = "";
-    box.removeAttribute("disabled");
-  }
-  winnerPlayer.classList.add("hidden");
-  newGameBtn.classList.add("hidden");
-  // winnerNamePlate.classList.add("hidden");
-});
